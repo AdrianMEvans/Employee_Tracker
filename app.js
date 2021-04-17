f44b8962eb7ecb7868eaf477dddfbea8821cea39
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-04-17 17:21:09 +0800

    add department function

--- app.js
+++ app.js
@@ -315,63 +315,66 @@ function manageEmployee() {
 //MANAGE THE DEPARTMENTS
 function manageDepartment() {
     console.log("Manage Department!!");
-    
-        function addDepartment() {
-            console.log("Add Department!!");
 
-        main();
-        }
-        
-        function viewDepartment() {
-            let query = 
+    function addDepartment() {
+        console.log("\nAdd a department\n");
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is the department name?"
+                }
+            ]).then((res) => {
+                let query = `INSERT INTO department SET ?`;
+                connection.query(query, { name: res.name }, (err, res) => {
+                    if (err) throw err;
+                    main();
+                });
+            });
+    }
+
+    function viewDepartment() {
+        let query =
             `SELECT 
                 d.id, 
                 d.name
             FROM department d`
-            
-            connection.query(query, (err, res)=>{
-              if (err) throw err;
-              console.table(res);
-              main();
-            });
-        }
-        
 
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            main();
+        });
+    }
 
-        // function viewDepartment() {
-        //     console.log("View Department!!");
+    function departmentTasks(payload) {
 
-        // main();
-        // }
-    
-       
-        function departmentTasks(payload) {
-    
-            const taskNames = Object.keys(payload);
-            const choices = taskNames.map((task) => {
-                return {
-                    name: task,
-                    value: payload[task],
-                }
-            })
-        
-            inquirer.prompt([
-                {
-                    name: "taskSelected",
-                    message: "What would you like to do?",
-                    type: "list",
-                    choices: choices,
-                }
-            ]).then(({taskSelected}) => taskSelected())
+        const taskNames = Object.keys(payload);
+        const choices = taskNames.map((task) => {
+            return {
+                name: task,
+                value: payload[task],
             }
-        
-            function departmentMain() {
-                departmentTasks({
-                    "Add Department": addDepartment,
-                    "View Department": viewDepartment,
-                })        
+        })
+
+        inquirer.prompt([
+            {
+                name: "taskSelected",
+                message: "What would you like to do?",
+                type: "list",
+                choices: choices,
             }
-            departmentMain();
+        ]).then(({ taskSelected }) => taskSelected())
+    }
+
+    function departmentMain() {
+        departmentTasks({
+            "Add Department": addDepartment,
+            "View Department": viewDepartment,
+        })
+    }
+    departmentMain();
 }
 
 // INTRO TO MANAGING THE TABLES
