d63c609ade3d9cc9e3f9fa5db14499f3845941a8
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-04-17 17:22:11 +0800

    updated manag dept file

--- src/inquirer/manageDepartment.js
+++ src/inquirer/manageDepartment.js
@@ -3,52 +3,65 @@
 // MANAGE DEPARTMENT TABLE
 // function manageDepartment() {
 //     console.log("Manage Department!!");
-    
-//         function addDepartment() {
-//             console.log("Add Department!!");
 
-//         main();
-//         }
-        
-//         function viewDepartment() {
-//             console.log("View Department!!");
-
-//         main();
-//         }
-    
-//         function updateDepartment() {
-//             console.log("Update Department!!");
-
-//         main();
-//         }
-//         function departmentTasks(payload) {
-    
-//             const taskNames = Object.keys(payload);
-//             const choices = taskNames.map((task) => {
-//                 return {
-//                     name: task,
-//                     value: payload[task],
-//                 }
-//             })
-        
-//             inquirer.prompt([
+//     function addDepartment() {
+//         console.log("\nAdd a department\n");
+//         inquirer
+//             .prompt([
 //                 {
-//                     name: "taskSelected",
-//                     message: "What would you like to do?",
-//                     type: "list",
-//                     choices: choices,
+//                     type: "input",
+//                     name: "name",
+//                     message: "What is the department name?"
 //                 }
-//             ]).then(({taskSelected}) => taskSelected())
-//             }
-        
-//             function departmentMain() {
-//                 departmentTasks({
-//                     "Add Department": addDepartment,
-//                     "View Department": viewDepartment,
-//                     "Update Department": updateDepartment,
-//                 })        
-//             }
-//             departmentMain();
-// }
+//             ]).then((res) => {
+//                 let query = `INSERT INTO department SET ?`;
+//                 connection.query(query, { name: res.name }, (err, res) => {
+//                     if (err) throw err;
+//                     main();
+//                 });
+//             });
+//     }
 
+//     function viewDepartment() {
+//         let query =
+//             `SELECT 
+//                 d.id, 
+//                 d.name
+//             FROM department d`
+
+//         connection.query(query, (err, res) => {
+//             if (err) throw err;
+//             console.table(res);
+//             main();
+//         });
+//     }
+
+//     function departmentTasks(payload) {
+
+//         const taskNames = Object.keys(payload);
+//         const choices = taskNames.map((task) => {
+//             return {
+//                 name: task,
+//                 value: payload[task],
+//             }
+//         })
+
+//         inquirer.prompt([
+//             {
+//                 name: "taskSelected",
+//                 message: "What would you like to do?",
+//                 type: "list",
+//                 choices: choices,
+//             }
+//         ]).then(({ taskSelected }) => taskSelected())
+//     }
+
+//     function departmentMain() {
+//         departmentTasks({
+//             "Add Department": addDepartment,
+//             "View Department": viewDepartment,
+//         })
+//     }
+//     departmentMain();
+// }
 // module.exports = manageDepartment;
